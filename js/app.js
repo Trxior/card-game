b098755205c4bcdf509099ac6bca529dbf9dd453
Trix <trxior@gmail.com>
2021-01-22 13:08:32 +0100

    fix 100 vh

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    
+    window.scrollTo(0,1);
 
     let cards = {
             opponent: {
